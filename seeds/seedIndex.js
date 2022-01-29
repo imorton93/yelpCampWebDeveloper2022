bbdcadea14e870a719b955d7dde6d01c5af2a7c9
imorton <imorton93@live.ca>
2022-01-29 01:49:06 -0800

    images seeds added to campgrounds. Show now displays images of campgrounds

--- seeds/seedIndex.js
+++ seeds/seedIndex.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useUnifiedTopology: true
 })
 
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once('open', () => {
@@ -27,11 +28,14 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'lorem ipsum dolor sit amet consectetur adipisicing elit',
+            price
         })
-        console.log(`${sample(descriptors)} ${sample(places)}`)
         await camp.save();
     }
 }
