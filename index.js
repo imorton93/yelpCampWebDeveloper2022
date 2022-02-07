f808eab6c9e1c513c051ee82994cd76c97bb2416
imorton <imorton93@live.ca>
2022-02-07 00:11:57 -0800

    review routes established

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Campground = require('./models/campground');
 const req = require('express/lib/request');
 const Joi = require('joi');
 const {campgroundSchema} = require('./schemas.js') 
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -90,6 +91,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } =  req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review =  new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req, res, next) => {
